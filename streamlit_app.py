0ddce0eb1dbe5ca813d65aff61ad798cda118b32
anushareddynagaram <anusha@scaeleiq.com>
2026-06-20 20:38:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,44 @@
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom smoothie!")
 
-name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your smoothie will be:', name_on_order)
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your smoothie will be:", name_on_order)
 
-# Updated Snowflake connection
-cnx = st.connection("snowflake")
-session = cnx.session()
+session = get_active_session()
 
 my_dataframe = session.table(
     "smoothies.public.fruit_options"
-).select(col('FRUIT_NAME'))
+).select(col("FRUIT_NAME"))
+
+fruit_list = [row["FRUIT_NAME"] for row in my_dataframe.collect()]
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    my_dataframe,
+    "Choose up to 5 ingredients:",
+    fruit_list,
     max_selections=5
 )
 
 if ingredients_list:
-    ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+    ingredients_string = " ".join(ingredients_list)
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """
-    insert into smoothies.public.orders(ingredients, name_on_order)
-    values ('""" + ingredients_string + """','""" + name_on_order + """')
-    """
+    if st.button("Submit Order"):
 
-    time_to_insert = st.button("Submit Order")
+        insert_stmt = f"""
+        INSERT INTO smoothies.public.orders
+        (ingredients, name_on_order)
+        VALUES ('{ingredients_string}', '{name_on_order}')
+        """
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        session.sql(insert_stmt).collect()
+
         st.success(
-            'Your Smoothie is ordered, ' + name_on_order + '!',
+            f"Your Smoothie is ordered, {name_on_order}!",
             icon="✅"
         )
