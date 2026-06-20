df42fb2f50a2fb26d09b69e7d6ffa7935e3292d6
anushareddynagaram <anusha@scaeleiq.com>
2026-06-20 22:55:31 +0530

    Update streamlit_app.py
    Added API call to fetch fruit data and display it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
+import requests
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom smoothie!")
@@ -8,7 +9,6 @@ st.write("Choose the fruits you want in your custom smoothie!")
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
-# ✅ Correct way: use secret NAMES, not values
 connection_parameters = {
     "account": st.secrets["account"],
     "user": st.secrets["user"],
@@ -21,7 +21,6 @@ connection_parameters = {
 
 session = Session.builder.configs(connection_parameters).create()
 
-# Get fruit list from Snowflake
 my_dataframe = session.table(
     "SMOOTHIES.PUBLIC.FRUIT_OPTIONS"
 ).select(col("FRUIT_NAME"))
@@ -29,31 +28,24 @@ my_dataframe = session.table(
 fruit_list = [row["FRUIT_NAME"] for row in my_dataframe.collect()]
 
 ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients:",
-    fruit_list,
-    max_selections=5
+    "Choose up to 5 ingredients:", fruit_list, max_selections=5
 )
 
 if ingredients_list:
-
     ingredients_string = ", ".join(ingredients_list)
-
     st.write("Selected:", ingredients_string)
 
-    if st.button("Submit Order"):
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response.json())
 
+    if st.button("Submit Order"):
         safe_name = name_on_order.replace("'", "''")
         safe_ingredients = ingredients_string.replace("'", "''")
-
         insert_stmt = f"""
-        INSERT INTO SMOOTHIES.PUBLIC.ORDERS
-        (INGREDIENTS, NAME_ON_ORDER)
-        VALUES ('{safe_ingredients}', '{safe_name}')
+            INSERT INTO SMOOTHIES.PUBLIC.ORDERS (INGREDIENTS, NAME_ON_ORDER)
+            VALUES ('{safe_ingredients}', '{safe_name}')
         """
-
         session.sql(insert_stmt).collect()
-
         st.success(
-            f"Your Smoothie is ordered, {name_on_order}!",
-            icon="✅"
+            f"Your Smoothie is ordered, {name_on_order}!", icon="✅"
         )
